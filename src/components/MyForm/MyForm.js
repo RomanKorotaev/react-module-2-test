57748fc8a752ae4742ba2c045631ed1adfb32be5
Roman Korotaev <kro1986@ukr.net>
2021-10-10 23:05:59 +0300

    Ошибка при обновлении исходных данных после сабмита формы

--- src/components/MyForm/MyForm.js
+++ src/components/MyForm/MyForm.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import s from './MyForm.module.css';
 import shortid from 'shortid'
+import initialTodos2 from '../../todos2.json'
+import AppTest from '../../App'
 
 class MyForm extends Component {
 
     state = {
         value: '123',
+        id:'',
         text: '',
         completed: false,
         tag: '',
     };
+
+
 
     idInputeId = shortid.generate();
     textInputeId = shortid.generate();
@@ -27,9 +32,33 @@ class MyForm extends Component {
 
         console.log ("e.currentTarget", e.target)
          console.log ("e.currentTarget.name", e.currentTarget.name )
-        console.log ("e.currentTarget.value", e.currentTarget.value )
-        
+        console.log ("e.currentTarget.value", e.currentTarget.value ) 
     }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log ('Сработала отправка формы')
+            
+         let tmpObj = {
+            id: this.state.id, text: this.state.text, completed:this.state.completed, tag: this.state.tag 
+            }
+
+//         JSON.stringify(tmpObj);
+//         console.log ("tmpObj ",tmpObj)
+
+// //         console.log("initialTodos2 до пуша", initialTodos2)
+// //         initialTodos2.push(tmpObj);
+// //  console.log ("initialTodos2 после пуша",  initialTodos2)
+
+//         AppTest.setState(
+//             prevState => ({
+//                 todos2: tmpObj,
+//             }));
+        
+//          console.log("Вызов обновления")
+ 
+        
+     }
 
     handleCheckBox = e => {
         console.log("e.currentTarget.checked", e.target.checked)
@@ -47,7 +76,7 @@ class MyForm extends Component {
                 <p className=''>Велью:  {value} </p>
                 <br></br>
 
-                <form className={s.MyForm}>
+                <form className={s.MyForm} onSubmit={this.handleSubmit}>
                     <h3> Введите данные в форму: </h3>
                     
                      <label htmlFor={this.idInputeId}>
@@ -65,7 +94,7 @@ class MyForm extends Component {
                         <input type="text"
                             name ="text"
                             onChange={this.handleInputChange}
-                            value={this.state.tag}
+                            value={this.state.text}
                             id={this.textInputeId}
                         />
                     </label>
@@ -75,19 +104,20 @@ class MyForm extends Component {
                         <input type="text"
                             name="tag"
                             onChange={this.handleInputChange}
-                            value={this.state.text}
+                            value={this.state.tag}
                             id={this.tagInputeId}
                         />
                     </label> 
 
                     <input type="checkbox" name="completed" checked={this.state.completed} onChange={ this.handleCheckBox}/> Изучили технологию или нет?
-
+<button type="submit"> Отправить форму </button>
                 </form>
 
                 <br></br>
             </div>
      )   
     }
- }
+}
+ 
 
 export default MyForm;
